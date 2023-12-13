0aa47cff5cd394fb742c24a8136f11c3a48c4045
Hatim Shah <hs1180@georgetown.edu>
2023-12-12 19:30:08 -0500

    liapp 2

--- liapp.py
+++ liapp.py
@@ -93,11 +93,85 @@ else:
 
 
 
-st.button("Submit")
 
 
 
-st.write("You are a LinkedIN user")
+
+
+
+
+
+s = pd.read_csv(r'C:\Users\zooke\Desktop\Final Project\social_media_usage.csv')
+
+
+import sklearn
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+ss = pd.DataFrame({
+    "sm_li":np.where(s["web1h"] == 1, 1, 0),
+    "Income":np.where(s["income"]==9, np.nan, s["income"]),
+    "Age":np.where(s["age"]>98, np.nan, s["age"]),
+    "education":np.where(s["educ2"] >8,np.nan, s["educ2"]),
+    "marital":np.where(s["marital"] == 1, 1, 0),
+    "Parent":np.where(s["par"]==1,1,0),
+    "female":np.where(s["gender"] == 2, 1, 0)})
+
+ss.dropna(inplace=True)
+
+
+
+# Target (y) and feature(s) selection (X)
+y = ss["sm_li"]
+X = ss[["Age", "education", "Income", "female", "Parent"]]
+
+# Split data into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    stratify=y,       # same number of target in training & test set
+                                                    test_size=0.2,    # hold out 20% of data for testing
+                                                    random_state=987) # set for reproducibility
+
+# X_train contains 80% of the data and contains the features used to predict the target when training the model. 
+# X_test contains 20% of the data and contains the features used to test the model on unseen data to evaluate performance. 
+# y_train contains 80% of the the data and contains the target that we will predict using the features when training the model. 
+# y_test contains 20% of the data and contains the target we will predict when testing the model on unseen data to evaluate performance.
+
+
+# Initialize algorithm 
+lr = LogisticRegression(class_weight="balanced")
+
+# Fit algorithm to training data
+lr.fit(X_train, y_train)
+
+# Make predictions using the model and the testing data
+y_pred = lr.predict(X_test)
+
+
+
+
+# New data for features: age, education, Income, female, Parent
+person = [Age, education, Income, female, Parent]
+
+# Predict class, given input features
+predicted_class = lr.predict([person])
+
+# Generate probability of positive class (=1)
+probs = lr.predict_proba([person])
+
+
+st.write(f"Prediction: {predicted_class[0]}")
+st.write("(1 = LinkedIn User, 0 = Not a LinkedIn User)")
+
+st.write(f"Probability that you are a LinkedIn User: {probs[0][1]}")
+
+
+
+
+
+
 
 
 
